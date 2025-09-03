06aa8957eabceeecfaeb13e482806dbaca3ec5b5
CokeKJ1 <jcpm10murillo@gmail.com>
2025-09-03 12:05:18 -0400

    Add files via upload

---
+++ Saludo.java
@@ -0,0 +1,10 @@
+package saludo;
+public class Saludo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Equipo 1!");
+        System.out.println("1. Jorge Christian Paredes Murillo");
+        System.out.println("2. Grover Chura Chura");
+        System.out.println("3. Juan Carlos Caritas Rocha");
+        System.out.println("4. Walter Ruben Huanquiri Gutierrez");
+    }
+}
